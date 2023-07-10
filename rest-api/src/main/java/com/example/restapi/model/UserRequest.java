24a5cb11e3fef00df530b1724b58c687f381611d
rookedsysc <rookedsysc36@gmail.com>
2023-07-11 08:43:31 +0900

    ObjectMapper를 이용한 Json 직렬화

--- rest-api/src/main/java/com/example/restapi/model/UserRequest.java
+++ rest-api/src/main/java/com/example/restapi/model/UserRequest.java
@@ -1,5 +1,7 @@
 package com.example.restapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -13,5 +15,16 @@ import lombok.NoArgsConstructor;
 public class UserRequest {
     private String userName;
     private String userEmail;
+    // 이와 같이 JsonProperty를 사용하여 Json Mapping을 직접 t정할 수 있다.
+    // 이건 위에서 선언한 JsonNaming을 무시한다.
+    @JsonProperty("userAge")
     private int age;
+
+
+    // Json Mapping 과정에서 예상치 못한 결과가 나올 수 있기 때문에
+    // JsonIgnore를 사용하여 무시하도록 한다.
+    @JsonIgnore
+    public String getUserName() {
+        return this.userName + " 입니다.";
+    }
 }
--- rest-api/src/test/java/com/example/restapi/RestApiApplicationTests.java
+++ rest-api/src/test/java/com/example/restapi/RestApiApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.restapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.restapi.model.UserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 class RestApiApplicationTests {
-
+	// Spring에서 관리하는 Bean을 주입받는다.
+	@Autowired
+	private ObjectMapper objectMapper;
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+		UserRequest user = new UserRequest("홍길동", "rookedsysc36@gmail.com", 30);
+		
+		var json = objectMapper.writeValueAsString(user);
+		// {"userName":"홍길동","userEmail":"rookedsysc36@gmail.com","age":30,"nmae":"홍길동","personEmail":"rookedsysc36@gmail.com","bornIn":30}
+		System.out.println(json);
 	}
-
 }
