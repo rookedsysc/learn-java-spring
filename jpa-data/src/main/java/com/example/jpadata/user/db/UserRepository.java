e4ee16551f5beb58e4658d34c04c58a355e589b9
rookedsysc <rookedsysc36@gmail.com>
2023-07-24 00:11:39 +0900

    commit

--- jpa-data/src/main/java/com/example/jpadata/user/db/UserRepository.java
+++ jpa-data/src/main/java/com/example/jpadata/user/db/UserRepository.java
@@ -3,5 +3,4 @@ package com.example.jpadata.user.db;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> /* Entity / Id Type */{
-
 }
