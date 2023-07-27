b9c3342668fe469ab18038353cff23670a802403
rookedsysc <rookedsysc36@gmail.com>
2023-07-27 13:33:57 +0900

    DB랑 테이블 연동하기

---
+++ simple-board/src/main/java/com/example/simpleboard/post/db/PostRepository.java
@@ -0,0 +1,7 @@
+package com.example.simpleboard.post.db;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<PostEntity,Long> {
+}
