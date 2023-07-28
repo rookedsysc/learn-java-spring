60dcb408550489696ecc2b700a7801fa6980ebda
rookedsysc <rookedsysc36@gmail.com>
2023-07-28 22:12:04 +0900

    삭제된 Post 보이지 않게 수정

--- simple-board/backend/src/main/java/com/example/simpleboard/post/controller/PostApiController.java
+++ simple-board/backend/src/main/java/com/example/simpleboard/post/controller/PostApiController.java
@@ -2,14 +2,13 @@ package com.example.simpleboard.post.controller;
 
 import com.example.simpleboard.post.db.PostEntity;
 import com.example.simpleboard.post.model.PostRequest;
+import com.example.simpleboard.post.model.PostViewRequest;
 import com.example.simpleboard.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RequestMapping("/api/post")
 @RestController
@@ -20,5 +19,28 @@ public class PostApiController {
     @PostMapping("")
     public PostEntity create(@Valid @RequestBody PostRequest postRequest) {
         return postService.create(postRequest);
+    }
+
+    @PostMapping("/view")
+    public PostEntity view(
+            @Valid
+            @RequestBody PostViewRequest postViewRequest
+    ) {
+        return postService.view(postViewRequest);
+    }
+
+
+    @GetMapping("all")
+    public List<PostEntity> list() {
+        return postService.all();
+
+    }
+
+    @PostMapping("/delete")
+    public void delete(
+            @Valid
+            @RequestBody PostViewRequest postViewRequest
+    ) {
+        postService.delete(postViewRequest);
     }
 }
--- simple-board/backend/src/main/java/com/example/simpleboard/post/service/PostService.java
+++ simple-board/backend/src/main/java/com/example/simpleboard/post/service/PostService.java
@@ -3,11 +3,13 @@ package com.example.simpleboard.post.service;
 import com.example.simpleboard.post.db.PostEntity;
 import com.example.simpleboard.post.db.PostRepository;
 import com.example.simpleboard.post.model.PostRequest;
+import com.example.simpleboard.post.model.PostViewRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +28,49 @@ public class PostService {
                 .postedAt(LocalDateTime.now())
                 .build();
         return postRepository.save(entity);
+    }
+
+    /**
+     * 1. 게시글이 있는가?
+     * 2. 비밀번호가 맞는가?
+     */
+    public PostEntity view(PostViewRequest postViewRequest) {
+        return postRepository.findFirstByIdAndStatusOrderByIdDesc(postViewRequest.getPostId(), "REGISTERED")
+                .map(
+                        it -> {
+                            if (!it.getPassword()
+                                    .equals(postViewRequest.getPassword())) {
+                                var format = "패스워드가 맞지 않습니다. %s vs %s";
+                                throw new RuntimeException(String.format(format, it.getPassword(), postViewRequest.getPassword()));
+                            }
+                            return it;
+                        }
+                )
+                .orElseThrow(() -> {
+                    return new RuntimeException("해당 게시글이 존재하지 않습니다. : " + postViewRequest.getPostId());
+                });
+    }
+
+
+
+    public List<PostEntity> all() {
+        return postRepository.findAll();
+    }
+
+    public void delete(PostViewRequest postViewRequest) {
+        postRepository.findById(postViewRequest.getPostId()).map(
+                it -> {
+                    if(!it.getPassword().equals(postViewRequest.getPassword())) {
+                        var format = "패스워드가 맞지 않습니다. %s vs %s";
+                        throw new RuntimeException(String.format(format, it.getPassword(), postViewRequest.getPassword()));
+                    }
+                    it.setStatus("UNREGISTERED");
+                    postRepository.save(it);
+                    return it;
+                }
+        ).orElseThrow(() -> {
+            return new RuntimeException("해당 게시글이 존재하지 않습니다. : " + postViewRequest.getPostId());
+        });
+
     }
 }
