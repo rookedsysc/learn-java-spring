b2a1f2e30bb90ca57646258149b17580fcced8b2
rookedsysc <rookedsysc36@gmail.com>
2023-10-27 00:21:01 +0900

    김영한님 Spring 기본 강의 프로젝트 생성 #9

--- hello.spring/src/main/java/hello/hello/spring/Application.java
+++ hello.spring/src/main/java/hello/hello/spring/Application.java
@@ -1,13 +1,13 @@
-package com.example.hellospring;
+package hello.hello.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloSpringApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
