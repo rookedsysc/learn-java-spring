70dec983831888ba27a40a8defca81b8a2acc62f
rookedsysc <rookedsysc36@gmail.com>
2023-10-27 01:26:14 +0900

    feat Response Body 사용해서 Json으로 데이터 처리 #9

--- hello.spring/src/main/java/hello/hello/spring/controller/HelloController.java
+++ hello.spring/src/main/java/hello/hello/spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,4 +24,33 @@ public class HelloController {
     return "hello-template";
   }
 
+  /// 템플릿 엔진에 넣어주는게 아니라 http body에 데이터를 직접 넣어주겠다는 의미
+  /// 웹에서 보면 그냥 쌩 문자열이 들어있음
+  @GetMapping("hello-string")
+  /// http body에 데이터를 넣어주겠다느 의미
+  @ResponseBody
+  public String helloString(@RequestParam("name") String name) {
+    return "hello " + name; // "hello spring"
+  }
+
+  /// Json으로 데이터 내려줌
+  @GetMapping("hello-api")
+  @ResponseBody
+  public Hello helloApi(@RequestParam("name") String name) {
+    Hello hello = new Hello();
+    hello.setName(name);
+    return hello;
+  }
+
+  static class Hello {
+    private String name;
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
 }
