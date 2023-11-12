711a893d27cf33d59f9ed79f14ea40a8c9c1db81
rookedsysc <rookedsysc36@gmail.com>
2023-11-13 05:57:50 +0900

    feat Redis Hash + Java 사용법 #11

--- redis/redis-hash/src/main/java/org/example/Main.java
+++ redis/redis-hash/src/main/java/org/example/Main.java
@@ -1,7 +1,43 @@
 package org.example;
 
+import redis.clients.jedis.JedisPool;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    try (var jedisPool = new JedisPool("localhost", 6379)) {
+      try (
+          var jedis = jedisPool.getResource();
+      ) {
+        String key = "user:2:info";
+        String key2 = "user:3:info";
+
+        // 1-1. hset : key, field, value
+        jedis.hset(key2, "name", "greg2");
+        // 1-2. hset : key, Map
+        var map = new HashMap<String, String>();
+        map.put("name", "greg3");
+        map.put("email", "greg3@gmail.com");
+        map.put("phone-number", "010-1234-5678");
+        map.put("age", "2");
+        jedis.hset(key, map);
+
+        // 2. hdel : key, field
+        jedis.hdel(key, "phone-number");
+
+        // 3. hget : key, field
+        System.out.println(jedis.hget(key, "name"));
+
+        // 4. hgetall : key
+        Map<String,String> userInfo = jedis.hgetAll(key);
+        userInfo.forEach((k, v) -> System.out.println(k + " : " + v));
+
+        // 5. hincrby : key, field, increment
+        jedis.hincrBy(key, "age", 1);
+        System.out.println("age value changed : " +  jedis.hget(key, "age"));
+      }
+    }
   }
 }
