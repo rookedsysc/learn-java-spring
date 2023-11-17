4d755cb2bc377e6647d619a39b35ab4dad72d6e5
rookedsysc <rookedsysc36@gmail.com>
2023-11-17 19:47:07 +0900

    feat UserRepository 생성 및 Application 시작시 데이터 추가 #12

--- jediscache/src/main/java/com/example/jediscache/JediscacheApplication.java
+++ jediscache/src/main/java/com/example/jediscache/JediscacheApplication.java
@@ -1,14 +1,39 @@
 package com.example.jediscache;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JediscacheApplication {
+@RequiredArgsConstructor
+public class JediscacheApplication implements ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JediscacheApplication.class, args);
-	}
+  private final UserRepository userRepository;
+
+  public static void main(String[] args) {
+    SpringApplication.run(JediscacheApplication.class, args);
+  }
+
+  @Override
+	/// Application이 시작할 때 실행되는 메소드
+  public void run(ApplicationArguments args) throws Exception {
+    userRepository.save(User.builder().
+        name("greg").
+        email("greg@gmail.com")
+        .build());
+    userRepository.save(User.builder().
+        name("tom").
+        email("tom@gmail.com")
+        .build());
+    userRepository.save(User.builder().
+        name("ryan").
+        email("ryan@gmail.com")
+        .build());
+    userRepository.save(User.builder().
+        name("bob").
+        email("bob@gmail.com")
+        .build());
+  }
 }
