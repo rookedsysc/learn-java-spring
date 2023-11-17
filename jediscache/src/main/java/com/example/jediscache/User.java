c233da9479bec097fe268298eeab840e02373c3a
rookedsysc <rookedsysc36@gmail.com>
2023-11-17 19:46:34 +0900

    feat User Entity 생성 #12

---
+++ jediscache/src/main/java/com/example/jediscache/User.java
@@ -0,0 +1,34 @@
+package com.example.jediscache;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(length = 100)
+  private String email;
+  @Column(length = 30)
+  private String name;
+  @Column(name = "created_at")
+  @CreatedDate
+  private LocalDateTime createdAt;
+  @Column(name = "updated_at")
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+}
