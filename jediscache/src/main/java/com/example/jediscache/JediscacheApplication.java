d6a3ad89e3bf698d1d60091c6740d3034cef32e8
rookedsysc <rookedsysc36@gmail.com>
2023-11-17 19:17:35 +0900

    jedis cache project 생성

---
+++ jediscache/src/main/java/com/example/jediscache/JediscacheApplication.java
@@ -0,0 +1,14 @@
+package com.example.jediscache;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JediscacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JediscacheApplication.class, args);
+	}
+}
