b321c5465e9e57d5ff429188b78b66bf84524970
rookedsysc <rookedsysc36@gmail.com>
2023-11-17 20:54:34 +0900

    modify Cache Aside Pattern 방식으로 캐싱 #12

--- jediscache/src/main/java/com/example/jediscache/UserController.java
+++ jediscache/src/main/java/com/example/jediscache/UserController.java
@@ -4,19 +4,37 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
   private final UserRepository userRepository;
+  private final JedisPool jedisPool;
 
   @GetMapping("/users/{id}/email")
   public String getUserEmail(@PathVariable Long id) {
-    return userRepository.findById(id)
-        .orElse(
-            User.builder()
-                .build()
-        )
-        .getEmail();
+    try(Jedis jedis = jedisPool.getResource()) {
+      // 1. request to cache
+      String key = "user:" + id + ":email";
+      String userEmail = jedis.get(key);
+      if (userEmail != null) {
+        return userEmail;
+      }
+
+      // 2. else db
+      userEmail = userRepository.findById(id)
+          .orElse(
+              User.builder()
+                  .build()
+          )
+          .getEmail();
+
+      // 3. set cache
+      // * 30초간 캐시 유지되게 함 (TTL 설정)
+      jedis.setex(key, 30,userEmail);
+      return userEmail;
+    }
   }
 }
