568e1efed5bdc17e773693597c5984945ef46e70
rookedsysc <rookedsysc36@gmail.com>
2023-11-26 15:27:05 +0900

    Spring Session Config + Session Count 구현 #14

--- session-with-redis/src/main/java/com/example/sessionwithredis/SessionWithRedisApplication.java
+++ session-with-redis/src/main/java/com/example/sessionwithredis/SessionWithRedisApplication.java
@@ -1,8 +1,14 @@
 package com.example.sessionwithredis;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@RestController
 @SpringBootApplication
 public class SessionWithRedisApplication {
 
@@ -10,4 +16,13 @@ public class SessionWithRedisApplication {
 		SpringApplication.run(SessionWithRedisApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public Map<String, String> home(HttpSession session) {
+		Integer visitCount = (Integer) session.getAttribute("visits");
+		if(visitCount == null) {
+			visitCount = 0;
+		}
+		session.setAttribute("visits", ++visitCount);
+		return Map.of("session id", session.getId(), "visits", visitCount.toString());
+	}
 }
