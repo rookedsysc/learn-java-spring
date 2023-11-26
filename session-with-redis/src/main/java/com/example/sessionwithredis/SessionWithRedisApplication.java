19e2efe796030c4cb8ea28380718922d1b11fdd3
rookedsysc <rookedsysc36@gmail.com>
2023-11-26 14:46:48 +0900

    Project Create

---
+++ session-with-redis/src/main/java/com/example/sessionwithredis/SessionWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.sessionwithredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionWithRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionWithRedisApplication.class, args);
+	}
+
+}
---
+++ session-with-redis/src/test/java/com/example/sessionwithredis/SessionWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sessionwithredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionWithRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
