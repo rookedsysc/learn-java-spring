295f1e57f0800c6865e4024dcad871bd1958657c
rookedsysc <rookedsysc36@gmail.com>
2024-02-16 04:48:08 +0900

    쿠폰 시스템 Initial Commit

---
+++ coupon-system/src/test/java/org/couponsystem/CouponSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.couponsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
