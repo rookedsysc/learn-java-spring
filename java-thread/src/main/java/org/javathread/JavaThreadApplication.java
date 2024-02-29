d020c0a21a2a561f9deabcd327f6395688f7d0f4
rookedsysc <rookedsysc36@gmail.com>
2024-03-01 02:18:44 +0900

    Thread 완료까지 기다리기 wait()

--- java-thread/src/main/java/org/javathread/JavaThreadApplication.java
+++ java-thread/src/main/java/org/javathread/JavaThreadApplication.java
@@ -6,17 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaThreadApplication {
 
     public static void main(String[] args) {
-        MyThread1 myThread1 = new MyThread1();
-        myThread1.start();
-        System.out.println("Hello, My Child");
-
-        Thread myThread2 = new Thread(new MyThread2());
-        myThread2.start();
+//        MyThread1 myThread1 = new MyThread1();
+//        myThread1.start();
+//        System.out.println("Hello, My Child");
+//
+//        Thread myThread2 = new Thread(new MyThread2());
+//        myThread2.start();
 
         Runnable task = () -> {
             try {
-                while (true) {
-                    System.out.println("Hello, MyTask");
+                for(int i=0; i<5; i++) {
+                    System.out.println("Hello, MyThread3");
                     Thread.sleep(500); // miliseconds, 0.5초
                 }
             } catch (InterruptedException e) {
@@ -25,5 +25,14 @@ public class JavaThreadApplication {
         };
         Thread myThread3 = new Thread(task);
         myThread3.start();
+
+        try {
+            // thread는 wait()이 아니라 join()을 사용해서 대기한다.
+            myThread3.join(); // myThread3이 종료될 때까지 대기
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted");
+        }
+
+        System.out.println("Task Thread is Done");
     }
 }
