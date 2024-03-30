a80c3b59de446f67862352fed0aa308b2d57a880
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2024-03-30 15:57:13 +0900

    Query Project, Projection 활용 예제  (#32)
    * Projection 활용하는 다양한 방법
    
    * Query Projection 사용예제

---
+++ query-dsl/src/test/java/org/querydsl/ProjectionTest.java
@@ -0,0 +1,108 @@
+package org.querydsl;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.querydsl.dto.MemberDto;
+import org.querydsl.dto.QMemberDto;
+import org.querydsl.entity.Member;
+import org.querydsl.entity.Team;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.querydsl.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class ProjectionTest {
+    @PersistenceContext
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    void setUp() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+    }
+
+    @Test
+    void useSetter() {
+        List<MemberDto> result = queryFactory.select(Projections.bean(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    void useField() {
+        List<MemberDto> result = queryFactory.select(Projections.fields(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    @DisplayName("필드명이 달라서 별칭을 지정해줘야 할 때")
+    void useAsKeyword() {
+        List<MemberDto> result = queryFactory.select(Projections.fields(MemberDto.class,
+                        member.username.as("name"),
+                        member.age.as("age")))
+                .from(member)
+                .fetch();
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    void useConstructor() {
+        List<MemberDto> result = queryFactory.select(Projections.constructor(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    @DisplayName("Query Projection 테스트")
+    void queryProjection() {
+        List<MemberDto> result= queryFactory.select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+    }
+}
