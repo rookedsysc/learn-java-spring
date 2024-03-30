9a0d49374b048d8b16dc7a74d819e65981587ab5
rookedsysc <rookedsysc36@gmail.com>
2024-03-31 04:06:03 +0900

    feat Pagination Controller 추가

--- query-dsl-pagination-example/src/main/java/org/querydslpaginationexample/infrastructure/controller/MemberController.java
+++ query-dsl-pagination-example/src/main/java/org/querydslpaginationexample/infrastructure/controller/MemberController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.querydslpaginationexample.infrastructure.MemberRepository;
 import org.querydslpaginationexample.infrastructure.model.MemberSearchCondition;
 import org.querydslpaginationexample.infrastructure.model.MemberTeamDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,10 @@ public class MemberController {
     @GetMapping("/v1/members")
     public List<MemberTeamDto>searchMemberV1(MemberSearchCondition memberSearchCondition) {
         return memberRepository.search(memberSearchCondition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition memberSearchCondition, Pageable pageable) {
+        return memberRepository.searchPage(memberSearchCondition, pageable);
     }
 }
