8e7b37da0636462ed46ad5e1cb25934d32633f23
rookedsysc <rookedsysc36@gmail.com>
2024-03-30 14:58:47 +0900

    Projection 활용하는 다양한 방법

---
+++ query-dsl/src/main/java/org/querydsl/dto/MemberDto.java
@@ -0,0 +1,13 @@
+package org.querydsl.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+    private String name;
+    private int age;
+}
