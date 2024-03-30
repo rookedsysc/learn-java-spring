3e1934ca80114a7cbc9ab39c89fb857afdcd465a
rookedsysc <rookedsysc36@gmail.com>
2024-03-30 16:00:44 +0900

    project initial

---
+++ query-dsl-pagination-example/src/test/java/org/querydslpaginationexample/QueryDslPaginationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.querydslpaginationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslPaginationExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
