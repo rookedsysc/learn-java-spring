6cdad2efb5c2fdc043948bb787a32a4310c1f51c
rookedsysc <rookedsysc36@gmail.com>
2024-04-04 03:48:39 +0900

    feat Project Initialize

---
+++ github-api/src/test/kotlin/org/githubapi/GithubApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.githubapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
