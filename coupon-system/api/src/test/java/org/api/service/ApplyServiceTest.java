20c657c94de07f53681097e609770d152abc25f1
rookedsysc <rookedsysc36@gmail.com>
2024-04-05 03:58:36 +0900

    test Coupon 1개 발급 테스트

---
+++ coupon-system/api/src/test/java/org/api/service/ApplyServiceTest.java
@@ -0,0 +1,26 @@
+package org.api.service;
+
+import org.api.repository.CouponRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplyServiceTest {
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    public void 한번만응모() {
+        applyService.apply(1L);
+        assertEquals(1, couponRepository.count());
+    }
+
+
+
+}
