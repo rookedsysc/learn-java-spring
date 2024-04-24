63cd6f4c94f13e9788c1b5cecac9c4f5a3a9ee06
rookedsysc <rookedsysc36@gmail.com>
2024-04-25 00:55:47 +0900

    fix expected single matching bean but found 2 REDIS Bean 2개 곂치는 에러 Fix

--- coupon-system/api/src/main/java/org/api/repository/CouponCountRepository.java
+++ coupon-system/api/src/main/java/org/api/repository/CouponCountRepository.java
@@ -1,5 +1,6 @@
 package org.api.repository;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Repository;
 public class CouponCountRepository {
     private final RedisTemplate<String, Integer> redisTemplate;
 
-    public CouponCountRepository(RedisTemplate redisTemplate) {
+    public CouponCountRepository(@Qualifier("stringRedisTemplate") RedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
