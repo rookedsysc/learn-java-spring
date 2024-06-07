ec2c25b7437bee8bcf70ba9cc16296a685089182
rookedsysc <rookedsysc36@gmail.com>
2024-06-07 20:39:10 +0900

    feat: TestController 추가

---
+++ i18n-test/src/main/java/org/i18ntest/i18ntest/TestController.java
@@ -0,0 +1,29 @@
+package org.i18ntest.i18ntest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class TestController {
+
+    private final MessageSource messageSource;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return getMessage("say.hello");
+    }
+
+    private String getMessage(String code) {
+        return getMessage(code, null);
+    }
+
+    private String getMessage(String code, Object[] args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
+}
