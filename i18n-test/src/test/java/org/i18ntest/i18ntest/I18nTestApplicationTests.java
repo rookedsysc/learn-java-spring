3dcf740fc965ec6da921bf4473abc94ed58ecaef
rookedsysc <rookedsysc36@gmail.com>
2024-06-07 17:08:41 +0900

    first commit

---
+++ i18n-test/src/test/java/org/i18ntest/i18ntest/I18nTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.i18ntest.i18ntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
