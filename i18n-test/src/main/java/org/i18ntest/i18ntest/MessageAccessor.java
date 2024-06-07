b4e28d577ae2e98e10d2563243614407854c4afa
rookedsysc <rookedsysc36@gmail.com>
2024-06-07 20:51:22 +0900

    feat: MessageAccessor 분리

---
+++ i18n-test/src/main/java/org/i18ntest/i18ntest/MessageAccessor.java
@@ -0,0 +1,20 @@
+package org.i18ntest.i18ntest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+@Configuration
+@RequiredArgsConstructor
+public class MessageAccessor {
+    private final MessageSource messageSource;
+
+    public String getMessage(String code) {
+        return getMessage(code, null);
+    }
+
+    private String getMessage(String code, Object[] args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
+}
--- i18n-test/src/main/java/org/i18ntest/i18ntest/TestController.java
+++ i18n-test/src/main/java/org/i18ntest/i18ntest/TestController.java
@@ -1,8 +1,6 @@
 package org.i18ntest.i18ntest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @RequiredArgsConstructor
 public class TestController {
-
-    private final MessageSource messageSource;
+    private final MessageAccessor messageAccessor;
 
     @GetMapping("/hello")
     public String hello() {
-        return getMessage("say.hello");
-    }
-
-    private String getMessage(String code) {
-        return getMessage(code, null);
-    }
-
-    private String getMessage(String code, Object[] args) {
-        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+        return messageAccessor.getMessage("say.hello");
     }
 }
