58e77d0be49ae09b7997ea493fa40a5940a53598
rookedsysc <rookedsysc36@gmail.com>
2024-06-08 16:56:59 +0900

    modify: SQL Auto Increment 추가

--- jpa-json-performance-measurement/src/main/resources/sql/DDL.sql
+++ jpa-json-performance-measurement/src/main/resources/sql/DDL.sql
@@ -2,26 +2,25 @@ create database jpa_json_performance_test;
 
 use jpa_json_performance_test;
 
-create table jpa_post (
-    id bigint not null,
+create table jpa_post
+(
+    id      bigint not null auto_increment primary key,
     content varchar(255),
-    title varchar(255),
-    primary key (id)
+    title   varchar(255)
 );
 
-drop table vote;
-
-create table vote (
-    id bigint not null,
+create table vote
+(
+    id        bigint not null auto_increment primary key,
     member_id bigint,
-    vote boolean,
-    post_id bigint,
-    primary key (id)
+    vote      boolean,
+    post_id   bigint
 );
 
-CREATE TABLE json_post (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
+CREATE TABLE json_post
+(
+    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title   VARCHAR(255),
     content TEXT,
-    votes JSON
+    votes   JSON
 );
