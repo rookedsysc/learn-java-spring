413b2d3fd0466f632c065033b19c8a6de9ab3cec
rookedsysc <rookedsysc36@gmail.com>
2024-06-08 15:00:26 +0900

    first commit

---
+++ jpa-json-performance-measurement/src/main/java/org/i18ntest/jpajsonperformancemeasurement/JpaJsonPerformanceMeasurementApplication.java
@@ -0,0 +1,13 @@
+package org.i18ntest.jpajsonperformancemeasurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaJsonPerformanceMeasurementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaJsonPerformanceMeasurementApplication.class, args);
+    }
+
+}
